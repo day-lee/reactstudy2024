449267147107f3615c6b78044cbb43d5dafd21d2
Dayeon Lee <iamdayeonlee@gmail.com>
2024-07-23 13:36:49 +0100

    Add loops to make the squares

--- src/Tictactoe.js
+++ src/Tictactoe.js
@@ -31,24 +31,24 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next Plyaer: " + (xIsNext ? "X" : "O");
   }
 
+  const rows = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+
+  const boardRows = rows.map((row) => (
+    <div className="board-row">
+      {row.map((item) => (
+        <Square value={squares[item]} onSquareClick={() => handleClick(item)} />
+      ))}
+    </div>
+  ));
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {boardRows}
     </>
   );
 }
