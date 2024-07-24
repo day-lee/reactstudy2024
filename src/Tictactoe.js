8a4beda139c2b870add7341f960c86d5825392a4
Dayeon Lee <iamdayeonlee@gmail.com>
2024-07-24 20:34:58 +0100

    test

--- src/Tictactoe.js
+++ src/Tictactoe.js
@@ -1,17 +1,39 @@
 import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
+  // when value got a flag, add css
+  // const [isPaused, setIsPaused] = useState(false);
+  // className={`App-logo ${isPaused ? `paused` : " "}`}
+  //   const [isWon, setIsWon] = useState(false);
+  //   console.log(win);
+  //   if (win !== null && win.includes(value)) {
+  //     setIsWon(true);
+  //   } else {
+  //     setIsWon(false);
+  //   }
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
+
+  //   return (
+  //     <button className={`square ${isWon ? `win` : " "}`} onClick={onSquareClick}>
+  //       {value}
+  //     </button>
+  //   );
 }
 
 function Board({ xIsNext, squares, currentMove, onPlay }) {
   function handleClick(i) {
-    // check if square already got a value, then return to quit the function
-    if (squares[i] || calculateWinner(squares)) {
+    //You will call calculateWinner(squares) in the Board component’s handleClick function to check if a player has won.
+    //You can perform this check at the same time you check if a user has clicked a square that already has a X or and O.
+    //We’d like to return early in both cases:
+    // 함수 값이 있거나. string 그래서 true or null 어레이의 두번째 값을 비교해야할듯
+    const calculateResult = calculateWinner(squares)[1];
+    console.log("calculateResult");
+    console.log(calculateResult);
+    if (squares[i] || calculateResult) {
       return;
     }
     const nextSquares = squares.slice();
@@ -23,13 +45,22 @@ function Board({ xIsNext, squares, currentMove, onPlay }) {
     onPlay(nextSquares);
   }
 
-  const winner = calculateWinner(squares);
+  //const winner = calculateWinner(squares);
+  const [winningLines, winner] = calculateWinner(squares);
+  console.log(winningLines, winner); // [0, 3, 6]
+  //squares array has no null and winner false means draw
   let status;
   if (winner) {
     status = "Winner: " + winner;
+  } else if (!winner && !squares.includes(null)) {
+    status = "Result: Draw";
   } else {
     status = "Next Player: " + (xIsNext ? "X" : "O");
   }
+  // draw case
+  //  oxo
+  //  xxo
+  //  xox
 
   const rows = [
     [0, 1, 2],
@@ -40,7 +71,11 @@ function Board({ xIsNext, squares, currentMove, onPlay }) {
   const boardRows = rows.map((row) => (
     <div className="board-row">
       {row.map((item) => (
-        <Square value={squares[item]} onSquareClick={() => handleClick(item)} />
+        <Square
+          value={squares[item]}
+          onSquareClick={() => handleClick(item)}
+          win={winningLines}
+        />
       ))}
     </div>
   ));
@@ -130,9 +165,11 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      console.log(lines[i]);
-      return squares[a];
+      console.log(lines[i], squares[a]);
+      // winner is decided, send the a b c as value as array?
+      //return squares[a]; //string value'x'
+      return [lines[i], squares[a]];
     }
   }
-  return null;
+  return [null, null];
 }
